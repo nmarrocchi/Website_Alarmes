a368355fa6bd2db09a1565c343f0a6714cb8919b
Mon Nom d'utilisateur <nmarrocchi@la-providence.net>
2021-02-11 16:22:46 +0100

    Website Final ( without C++ )

--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
-let stateBull   = false
-  , stateIBM    = false
-  , stateApple  = false;
+let stateAll = false,
+    stateBull = false,
+    stateIBM = false,
+    stateApple = false;
 
 // -- Paramètres des boutons par défaut ----------------------------------------------------
-function Set_Default_Property(){
-    
+function Set_Default_Property() {
+
+    // -- Propriétée des boutons ( enable / disable )
+
+    document.getElementById("Desactivate_all").setAttribute("disabled", "disabled");
+    document.getElementById("Activate_all").setAttribute("enabled", "enabled");
+
     document.getElementById("Desactivate_Bull").setAttribute("disabled", "disabled");
     document.getElementById("Activate_Bull").setAttribute("enabled", "enabled");
 
@@ -14,93 +20,82 @@ function Set_Default_Property(){
     document.getElementById("Desactivate_Apple").setAttribute("disabled", "disabled");
     document.getElementById("Activate_Apple").setAttribute("enabled", "enabled");
 
+
+        // -- Couleur des boutons ( Texte / Background )
+
+    document.getElementById("Desactivate_all").style.backgroundColor = "#C4C4C4";
+    document.getElementById("Activate_all").style.backgroundColor = "white";
+    document.getElementById("Desactivate_all").style.color = "grey";
+    document.getElementById("Activate_all").style.color = "black";
+
     document.getElementById("Desactivate_Bull").style.backgroundColor = "#C4C4C4";
     document.getElementById("Activate_Bull").style.backgroundColor = "white";
-        document.getElementById("Desactivate_Bull").style.color = "grey";
-        document.getElementById("Activate_Bull").style.color = "black";
+    document.getElementById("Desactivate_Bull").style.color = "grey";
+    document.getElementById("Activate_Bull").style.color = "black";
 
     document.getElementById("Desactivate_IBM").style.backgroundColor = "#C4C4C4";
     document.getElementById("Activate_IBM").style.backgroundColor = "white";
-        document.getElementById("Desactivate_IBM").style.color = "grey";
-        document.getElementById("Activate_IBM").style.color = "black";
+    document.getElementById("Desactivate_IBM").style.color = "grey";
+    document.getElementById("Activate_IBM").style.color = "black";
 
     document.getElementById("Desactivate_Apple").style.backgroundColor = "#C4C4C4";
     document.getElementById("Activate_Apple").style.backgroundColor = "white";
-        document.getElementById("Desactivate_Apple").style.color = "grey";
-        document.getElementById("Activate_Apple").style.color = "black";
+    document.getElementById("Desactivate_Apple").style.color = "grey";
+    document.getElementById("Activate_Apple").style.color = "black";
 
 }
 
 
 // -- Fonctions toggle pour switch de bouton enable et disable
 
-    // -- Salle Bull
+// -- Toutes les Salles
 
-function toggleBull(){ 
-    document.getElementById("Desactivate_Bull").toggleAttribute("disabled");
-    document.getElementById("Activate_Bull").toggleAttribute("disabled");
+function toggleAll() {
+    document.getElementById("Desactivate_all").toggleAttribute("disabled");
+    document.getElementById("Activate_all").toggleAttribute("disabled");
 
-    stateBull = !stateBull
+    stateAll = !stateAll
 
-    console.log("Etat bull :", stateBull);
+    console.log("Etat All :", stateAll);
 
-    if(stateBull == true){
+    if (stateAll == true) {
+        document.getElementById("Desactivate_all").style.backgroundColor = "white";
+        document.getElementById("Activate_all").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Desactivate_all").style.color = "black";
+        document.getElementById("Activate_all").style.color = "grey";
+
         document.getElementById("Desactivate_Bull").style.backgroundColor = "white";
         document.getElementById("Activate_Bull").style.backgroundColor = "#C4C4C4";
         document.getElementById("Desactivate_Bull").style.color = "black";
         document.getElementById("Activate_Bull").style.color = "grey";
-    }
-    else if(stateBull == false){
-        document.getElementById("Desactivate_Bull").style.backgroundColor = "#C4C4C4";
-        document.getElementById("Activate_Bull").style.backgroundColor = "white";
-        document.getElementById("Desactivate_Bull").style.color = "grey";
-        document.getElementById("Activate_Bull").style.color = "black";
-    }
 
-}
-
-    // -- Salle IBM
-
-function toggleIBM(){
-    document.getElementById("Desactivate_IBM").toggleAttribute("disabled");
-    document.getElementById("Activate_IBM").toggleAttribute("disabled");
-
-    stateIBM = !stateIBM;
-
-    console.log("Etat IBM :", stateIBM);
-
-    if(stateIBM == true){
         document.getElementById("Desactivate_IBM").style.backgroundColor = "white";
         document.getElementById("Activate_IBM").style.backgroundColor = "#C4C4C4";
         document.getElementById("Desactivate_IBM").style.color = "black";
         document.getElementById("Activate_IBM").style.color = "grey";
-    }
-    else if(stateIBM == false){
-        document.getElementById("Desactivate_IBM").style.backgroundColor = "#C4C4C4";
-        document.getElementById("Activate_IBM").style.backgroundColor = "white";
-        document.getElementById("Desactivate_IBM").style.color = "grey";
-        document.getElementById("Activate_IBM").style.color = "black";
-    }grey
 
-}
-
-    // -- Salle Apple
-
-function toggleApple(){
-    document.getElementById("Desactivate_Apple").toggleAttribute("disabled");
-    document.getElementById("Activate_Apple").toggleAttribute("disabled");
-
-    stateApple = !stateApple;
-
-    console.log("Etat Apple :", stateApple);
-
-    if(stateApple == true){
         document.getElementById("Desactivate_Apple").style.backgroundColor = "white";
         document.getElementById("Activate_Apple").style.backgroundColor = "#C4C4C4";
         document.getElementById("Desactivate_Apple").style.color = "blgreyack";
         document.getElementById("Activate_Apple").style.color = "grey";
-    }
-    else if(stateApple == false){
+
+        
+    } else if (stateAll == false) {
+        document.getElementById("Desactivate_all").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_all").style.backgroundColor = "white";
+        document.getElementById("Desactivate_all").style.color = "grey";
+        document.getElementById("Activate_all").style.color = "black";
+
+        document.getElementById("Desactivate_Bull").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_Bull").style.backgroundColor = "white";
+        document.getElementById("Desactivate_Bull").style.color = "grey";
+        document.getElementById("Activate_Bull").style.color = "black";
+
+        document.getElementById("Desactivate_IBM").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_IBM").style.backgroundColor = "white";
+        document.getElementById("Desactivate_IBM").style.color = "grey";
+        document.getElementById("Activate_IBM").style.color = "black";
+
         document.getElementById("Desactivate_Apple").style.backgroundColor = "#C4C4C4";
         document.getElementById("Activate_Apple").style.backgroundColor = "white";
         document.getElementById("Desactivate_Apple").style.color = "grey";
@@ -109,4 +104,75 @@ function toggleApple(){
 
 }
 
+// -- Salle Bull
 
+function toggleBull() {
+    document.getElementById("Desactivate_Bull").toggleAttribute("disabled");
+    document.getElementById("Activate_Bull").toggleAttribute("disabled");
+
+    stateBull = !stateBull
+
+    console.log("Etat bull :", stateBull);
+
+    if (stateBull == true) {
+        document.getElementById("Desactivate_Bull").style.backgroundColor = "white";
+        document.getElementById("Activate_Bull").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Desactivate_Bull").style.color = "black";
+        document.getElementById("Activate_Bull").style.color = "grey";
+    } else if (stateBull == false) {
+        document.getElementById("Desactivate_Bull").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_Bull").style.backgroundColor = "white";
+        document.getElementById("Desactivate_Bull").style.color = "grey";
+        document.getElementById("Activate_Bull").style.color = "black";
+    }
+
+}
+
+// -- Salle IBM
+
+function toggleIBM() {
+    document.getElementById("Desactivate_IBM").toggleAttribute("disabled");
+    document.getElementById("Activate_IBM").toggleAttribute("disabled");
+
+    stateIBM = !stateIBM;
+
+    console.log("Etat IBM :", stateIBM);
+
+    if (stateIBM == true) {
+        document.getElementById("Desactivate_IBM").style.backgroundColor = "white";
+        document.getElementById("Activate_IBM").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Desactivate_IBM").style.color = "black";
+        document.getElementById("Activate_IBM").style.color = "grey";
+    } else if (stateIBM == false) {
+        document.getElementById("Desactivate_IBM").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_IBM").style.backgroundColor = "white";
+        document.getElementById("Desactivate_IBM").style.color = "grey";
+        document.getElementById("Activate_IBM").style.color = "black";
+    }
+    grey
+
+}
+
+// -- Salle Apple
+
+function toggleApple() {
+    document.getElementById("Desactivate_Apple").toggleAttribute("disabled");
+    document.getElementById("Activate_Apple").toggleAttribute("disabled");
+
+    stateApple = !stateApple;
+
+    console.log("Etat Apple :", stateApple);
+
+    if (stateApple == true) {
+        document.getElementById("Desactivate_Apple").style.backgroundColor = "white";
+        document.getElementById("Activate_Apple").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Desactivate_Apple").style.color = "blgreyack";
+        document.getElementById("Activate_Apple").style.color = "grey";
+    } else if (stateApple == false) {
+        document.getElementById("Desactivate_Apple").style.backgroundColor = "#C4C4C4";
+        document.getElementById("Activate_Apple").style.backgroundColor = "white";
+        document.getElementById("Desactivate_Apple").style.color = "grey";
+        document.getElementById("Activate_Apple").style.color = "black";
+    }
+
+}
--- index.php
+++ index.php
@@ -5,29 +5,98 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="style.css" rel="stylesheet">
-        <title>Intéractions Salles</title>
+        <title>Interactions Salles</title>
     </head>
     <body>  
 
-    <div class="Banniere"><h1>Intéraction capteurs salles </h1></div>
+    <div class="Banniere"><h1>Interaction capteurs salles </h1></div>
+
+        <div class="Content">
+        <div class="all">
+                <button type="button" id="Activate_all" onclick="toggleAll()">Tous Ammorçer</button> 
+                <button type="button" id="Desactivate_all" onclick="toggleAll()">Tous Désammorçer</button>     
+            </div>
+            <div class="Bull">
+                <button type="button" id="Activate_Bull" onclick="toggleBull()">Ammorçer Bull</button> 
+                <button type="button" id="Desactivate_Bull" onclick="toggleBull()">Désammorçer Bull</button>     
+            </div>
+            <div class="IBM">
+                <button type="button" id="Activate_IBM" onclick="toggleIBM()">Ammorçer IBM</button>
+                <button type="button" id="Desactivate_IBM" onclick="toggleIBM()">Désammorçer IBM</button> 
+            </div>
+            <div class="Apple">
+                <button type="button" id="Activate_Apple" onclick="toggleApple()">Ammorçer Apple</button>   
+                <button type="button" id="Desactivate_Apple" onclick="toggleApple()">Désammorçer Apple</button>   
+            </div>            
+        </div>
 
         <div class="Content">
 
-            <div class="Bull">
-                    <button type="button" id="Desactivate_Bull" onclick="toggleBull()">Désammorçer Bull</button>    
-                    <button type="button" id="Activate_Bull" onclick="toggleBull()">Ammorçer Bull</button>  
+            <div class="Values_Bull">
+                <table>
+                    <th colspan="2">Capteurs en salle BULL</th>
+                    <tr>
+                        <td>Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Tamper Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>État Capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Continuité capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                </table>
             </div>
 
-            <div class="IBM">
-                    <button type="button" id="Desactivate_IBM" onclick="toggleIBM()">Désammorçer IBM</button>   
-                    <button type="button" id="Activate_IBM" onclick="toggleIBM()">Ammorçer IBM</button> 
+            <div class="Values_IBM">
+                <table>
+                <th colspan="2">Capteurs en salle IBM</th>
+                    <tr>
+                        <td>Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Tamper Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>État Capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Continuité capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                </table>
             </div>
 
-            <div class="Apple">
-                    <button type="button" id="Desactivate_Apple" onclick="toggleApple()">Désammorçer Apple</button> 
-                    <button type="button" id="Activate_Apple" onclick="toggleApple()">Ammorçer Apple</button>   
+            <div class="Values_Apple">
+                <table>
+                    <th colspan="2">Capteurs en salle Apple</th>
+                    <tr>
+                        <td>Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Tamper Présence : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>État Capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                    <tr>
+                        <td>Continuité capteurs : </td>
+                        <td>n/a</td>
+                    </tr>
+                </table>
             </div>
-            
         </div>
 
     <script src="functions.js"></script>
